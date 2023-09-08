e731e78cf5e047f8c88f92b6fee48f1c21feb219
Kattebu <butskoev@tut.by>
2023-09-08 12:18:02 +0300

    ch3

--- src/main/java/HW2/Classes/PromotionClient.java
+++ src/main/java/HW2/Classes/PromotionClient.java
@@ -6,14 +6,6 @@ public class PromotionClient extends Actor {
     private int participantCount;
     private String promotionName;
 
-    public String getPromotionName() {
-        return promotionName;
-    }
-
-    public void setPromotionName(String promotionName) {
-        this.promotionName = promotionName;
-    }
-
     public PromotionClient(String name, String promotionName, int id, int participantCount) {
         super(name);
         this.promotionName = promotionName;
@@ -21,47 +13,98 @@ public class PromotionClient extends Actor {
         this.participantCount = participantCount;
     }
 
+    /**
+     * Метод, возвращающий название акции
+     */
+    public String getPromotionName() {
+        return promotionName;
+    }
+
+    /**
+     * Метод, устанавливающий название акции
+     */
+    public void setPromotionName(String promotionName) {
+        this.promotionName = promotionName;
+    }
+
+    /**
+     * Метод, проверяющий, превышено ли количество участников акции
+     */
     public boolean isParticipantsLimit() {
         return participantCount > 10;
     }
-
+    /**
+     * Метод приинятия заказа
+     */
     public boolean isTakeOrder() {
         return super.isTakeOrder;
     }
-
+    /**
+     * Метод, проверяющий приинят ли заказ
+     */
     public void setTakeOrder(boolean isTakeOrder) {
         super.isTakeOrder = isTakeOrder;
     }
-
+    /**
+     * Метод осуществления заказа
+     */
     public boolean isMakeOrder() {
         return super.isMakeOrder;
     }
-
+    /**
+     * Метод, проверяющий осуществлен ли заказ
+     */
     public void setMakeOrder(boolean isMakeOrder) {
         super.isMakeOrder = isMakeOrder;
     }
 
     public Actor getActor() {
         return this;
-    };
+    }
+
+    /**
+     * Метод, возвращающий имя участника
+     */
 
     @Override
     public String getName() {
         return super.name;
 
     }
-
+    /**
+     * Метод, устанавливающий имя участника
+     */
     @Override
     public void setName(String name) {
         super.name = name;
 
     }
 
+    /**
+     * Метод, возвращающий id клиента
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Метод, устанавливающий id клиента
+     */
     public void setId(int id) {
         this.id = id;
+    }
+
+    /**
+     * Метод, возвращающий количество участников в акции
+     */
+    public int getParticipantCount() {
+        return participantCount;
+    }
+
+    /**
+     * Метод, устанавливающий количество участников в акции
+     */
+    public void setParticipantCount(int participantCount) {
+        this.participantCount = participantCount;
     }
 }
